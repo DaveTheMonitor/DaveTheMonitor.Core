4508a6a6b5244ff6533abe697c885b9e2495f9d1
DaveTheMonitor <72330548+DaveTheMonitor@users.noreply.github.com>
2024-06-22 22:29:18 -0400

    Added CoreBuilder project Removed DaveTheMonitor.CSR Updated solution platforms (removed AnyCPU)

--- DaveTheMonitor.CSR/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using DaveTheMonitor.Core.API;
-using DaveTheMonitor.CSR;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: Guid("5f987f80-ca3b-4b8a-a0b4-6036adc9a36b")]
-[assembly: AssemblyPluginEntry(typeof(CSRPlugin))]
