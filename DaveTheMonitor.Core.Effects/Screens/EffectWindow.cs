5a90f2f7141227eda89ad749dc4fa9daa8d09408
DaveTheMonitor <72330548+DaveTheMonitor@users.noreply.github.com>
2024-07-11 14:15:51 -0400

    Updated effects screen

---
+++ DaveTheMonitor.Core.Effects/Screens/EffectWindow.cs
@@ -0,0 +1,134 @@
+﻿using DaveTheMonitor.Core.Plugin;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StudioForge.Engine;
+using StudioForge.Engine.GUI;
+
+namespace DaveTheMonitor.Core.Effects.Screens
+{
+    internal sealed class EffectWindow : Window
+    {
+        private static ColorProfile _windowColors;
+        private static TextBox.ColorProfile _buffProgressBarColors;
+        private static TextBox.ColorProfile _debuffProgressBarColors;
+        private static TextBox.ColorProfile _neutralProgressBarColors;
+        private ActorEffect _effect;
+        private ProgressBar _durationBar;
+        private int _prevDuration;
+        private int _prevAge;
+
+        static EffectWindow()
+        {
+            _windowColors = StudioForge.TotalMiner.Colors.ButtonColors.Clone();
+            _windowColors.BackHoverColor = _windowColors.BackColor;
+            _windowColors.BackClickColor = _windowColors.BackColor;
+
+            _buffProgressBarColors = ProgressBar.DefaultColorProfile.Clone();
+            _buffProgressBarColors.BackColor = _windowColors.BackColor * 0.85f;
+            _buffProgressBarColors.BackColor.A = 255;
+            _buffProgressBarColors.BackHoverColor = _buffProgressBarColors.BackColor;
+            _buffProgressBarColors.BackClickColor = _buffProgressBarColors.BackColor;
+            _buffProgressBarColors.ForeColor = new Color(124, 179, 251);
+
+            _debuffProgressBarColors = _buffProgressBarColors.Clone();
+            _debuffProgressBarColors.ForeColor = new Color(247, 109, 119);
+
+            _neutralProgressBarColors = _buffProgressBarColors.Clone();
+            _neutralProgressBarColors.ForeColor = new Color(217, 217, 217);
+        }
+
+        public void UpdateDuration()
+        {
+            int duration = (int)_effect.Duration;
+            int age = (int)_effect.Age;
+            if (duration != _prevDuration || age != _prevAge)
+            {
+                _durationBar.Text = GetDurationString();
+            }
+            _durationBar.progress = GetProgress();
+            _prevDuration = duration;
+            _prevAge = age;
+        }
+
+        private string GetDurationString()
+        {
+            float totalTime = _effect.Duration - _effect.Age;
+            int minutes = (int)(totalTime / 60);
+            int seconds = (int)(totalTime % 60);
+            return minutes > 0 ? $"{minutes}m {seconds}s" : $"{seconds}s";
+        }
+
+        private float GetProgress()
+        {
+            return 1 - (_effect.Age / _effect.Duration);
+        }
+
+        private void InitWindows()
+        {
+            ActorEffectDefinition def = _effect.Definition;
+            int margin = 8;
+            int x = margin;
+            int y = margin;
+
+            int iconSize = Size.Y - margin - margin;
+#if DEBUG
+            CorePlugin.Log(_effect.Definition.BackgroundTexture?.ToString() ?? "Null");
+#endif
+            Window background = new Window(x, y, iconSize, iconSize);
+            background.Colors = TransparentColorProfile;
+            background.LoadTexture(_effect.Definition.BackgroundTexture, true, true, 1);
+            background.Texture.SrRect = def.GetBackgroundSrc(_effect);
+            AddChild(background);
+
+            Window icon = new Window(0, 0, background.Size.X, background.Size.Y);
+            icon.Colors = TransparentColorProfile;
+            icon.LoadTexture(_effect.Definition.IconTexture, true, true, 1);
+            icon.Texture.SrRect = def.GetIconSrc(_effect);
+            background.AddChild(icon);
+
+            x += iconSize + margin;
+            SpriteFont font = CoreGlobals.GameFont16;
+
+            string name = def.GetName(_effect);
+            Vector2 measure = font.MeasureString(name);
+            TextBox textBox = new TextBox(name, x, y, Size.X - x - margin, (int)measure.Y, 1, WinTextAlignX.Left, WinTextAlignY.Center);
+            textBox.Font = font;
+            textBox.Colors = StudioForge.TotalMiner.Colors.BlackText;
+            AddChild(textBox);
+
+            font = CoreGlobals.GameFont12;
+            y += (int)measure.Y;
+
+            string desc = def.GetDescription(_effect);
+            measure = font.MeasureString(desc);
+            textBox = new TextBox(desc, x, y, Size.X - x - margin, (int)measure.Y, 1, WinTextAlignX.Left, WinTextAlignY.Center);
+            textBox.Font = font;
+            textBox.Colors = StudioForge.TotalMiner.Colors.BlackText;
+            AddChild(textBox);
+
+            y = Size.Y - margin - 20;
+            _durationBar = new ProgressBar(x, y, Size.X - x - margin, 20);
+            _durationBar.Font = font;
+            _durationBar.TextAlignX = WinTextAlignX.Center;
+            _durationBar.TextAlignY = WinTextAlignY.Center;
+            _durationBar.Text = GetDurationString();
+            _durationBar.progress = GetProgress();
+            _durationBar.Colors = _effect.Definition.Type switch
+            {
+                ActorEffectType.Buff => _buffProgressBarColors,
+                ActorEffectType.Debuff => _debuffProgressBarColors,
+                _ => _neutralProgressBarColors
+            };
+            _durationBar.BorderThickness = 2;
+            AddChild(_durationBar);
+        }
+
+        public EffectWindow(ActorEffect effect, int x, int y, int width, int height) : base(x, y, width, height)
+        {
+            Colors = _windowColors;
+            BorderThickness = 0;
+            _effect = effect;
+            InitWindows();
+        }
+    }
+}
--- DaveTheMonitor.Core.Effects/Screens/EffectsScreen.cs
+++ DaveTheMonitor.Core.Effects/Screens/EffectsScreen.cs
@@ -1,111 +1,101 @@
 ﻿using DaveTheMonitor.Core.API;
 using DaveTheMonitor.Core.Gui;
-using DaveTheMonitor.Core.Plugin;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using StudioForge.Engine;
 using StudioForge.Engine.GUI;
-using StudioForge.TotalMiner;
 using StudioForge.TotalMiner.API;
+using System.Collections.Generic;
 
 namespace DaveTheMonitor.Core.Effects.Screens
 {
     public sealed class EffectsScreen : CoreGuiMenu
     {
-        private static Window.ColorProfile _windowColors;
         public override string Name => "Effects";
-
-        static EffectsScreen()
-        {
-            _windowColors = Colors.ButtonColors.Clone();
-            _windowColors.BackHoverColor = _windowColors.BackColor;
-            _windowColors.BackClickColor = _windowColors.BackColor;
-        }
+        private Dictionary<ActorEffect, EffectWindow> _windows;
+        private Window _container;
 
         protected override void InitWindows()
         {
             base.InitWindows();
+            _windows = new Dictionary<ActorEffect, EffectWindow>();
             InitMainContainer();
         }
 
         private void InitMainContainer()
         {
-            Window mainContainer = new Window();
-            mainContainer.Colors = Window.TransparentColorProfile;
-            mainContainer.Name = "main_container";
-            canvas.AddChild(mainContainer);
+            _container = new Window();
+            _container.Colors = Window.TransparentColorProfile;
+            _container.Name = "main_container";
+            canvas.AddChild(_container);
 
-            InitEffectWindows(mainContainer);
+            InitEffectWindows();
 
-            SetContainerToCanvas(mainContainer);
+            SetContainerToCanvas(_container);
         }
 
-        private void InitEffectWindows(Window container)
+        private void InitEffectWindows()
         {
-            int margin = 8;
-            int width = 600;
-            int height = 80;
-            int i = 0;
-            int startY = 34;
             ICoreMod mod = EffectsPlugin.Instance.Mod;
             foreach (ActorEffect effect in Player.Effects())
             {
-                Window win = CreateEffectWindow(effect, 0, startY + (i * height) + (i * margin), width, height);
-                container.AddChild(win);
-                i++;
+                AddEffectWindow(effect);
+            }
+
+            MoveWindows();
+        }
+        private void AddEffectWindow(ActorEffect effect)
+        {
+            EffectWindow win = new EffectWindow(effect, 0, 0, 600, 80);
+            _container.AddChild(win);
+            _windows.Add(effect, win);
+        }
+
+        private void MoveWindows()
+        {
+            int margin = 8;
+            int y = 34;
+            foreach (EffectWindow win in _windows.Values)
+            {
+                win.Position.Y = y;
+                y += win.Size.Y + margin;
             }
         }
 
-        private Window CreateEffectWindow(ActorEffect effect, int x, int y, int width, int height)
+        protected override void UpdateCore()
         {
-            ActorEffectDefinition def = effect.Definition;
-            int margin = 8;
-            int cX = margin;
-            int cY = margin;
+            try
+            {
+                foreach (EffectWindow win in _windows.Values)
+                {
+                    win.UpdateDuration();
+                }
+            }
+            catch { }
+        }
 
-            Window container = new Window(0, y, width, height);
-            container.Colors = _windowColors;
-            container.BorderThickness = 0;
+        protected override void OnOpen()
+        {
+            Player.Effects().EffectAdded += EffectAdded;
+            Player.Effects().EffectRemoved += EffectRemoved;
+        }
 
-            int iconSize = height - margin - margin;
-#if DEBUG
-            CorePlugin.Log(effect.Definition.BackgroundTexture?.ToString() ?? "Null");
-#endif
-            Window background = new Window(cX, cY, iconSize, iconSize);
-            background.Colors = Window.TransparentColorProfile;
-            background.LoadTexture(effect.Definition.BackgroundTexture, true, true, 1);
-            background.Texture.SrRect = def.GetBackgroundSrc(effect);
-            container.AddChild(background);
+        protected override void OnClose()
+        {
+            Player.Effects().EffectAdded -= EffectAdded;
+            Player.Effects().EffectRemoved -= EffectRemoved;
+        }
 
-            Window icon = new Window(0, 0, background.Size.X, background.Size.Y);
-            icon.Colors = Window.TransparentColorProfile;
-            icon.LoadTexture(effect.Definition.IconTexture, true, true, 1);
-            icon.Texture.SrRect = def.GetIconSrc(effect);
-            background.AddChild(icon);
+        private void EffectAdded(object sender, ActorEffectEventArgs e)
+        {
+            AddEffectWindow(e.Effect);
+            MoveWindows();
+        }
 
-            cX += iconSize + margin;
-            SpriteFont font = CoreGlobals.GameFont16;
-
-            string name = def.GetName(effect);
-            Vector2 measure = font.MeasureString(name);
-            TextBox textBox = new TextBox(name, cX, cY, width - cX - margin, (int)measure.Y, 1, WinTextAlignX.Left, WinTextAlignY.Center);
-            textBox.Text = name;
-            textBox.Font = font;
-            textBox.Colors = Colors.BlackText;
-            container.AddChild(textBox);
-
-            font = CoreGlobals.GameFont12;
-            cY += (int)measure.Y;
-
-            string desc = def.GetDescription(effect);
-            measure = font.MeasureString(desc);
-            textBox = new TextBox(desc, cX, cY, width - cX - margin, (int)measure.Y, 1, WinTextAlignX.Left, WinTextAlignY.Center);
-            textBox.Text = desc;
-            textBox.Font = font;
-            textBox.Colors = Colors.BlackText;
-            container.AddChild(textBox);
-
-            return container;
+        private void EffectRemoved(object sender, ActorEffectEventArgs e)
+        {
+            if (_windows.Remove(e.Effect, out EffectWindow win))
+            {
+                _container.RemoveChild(win);
+            }
+            MoveWindows();
         }
 
         public EffectsScreen(INewGuiMenuScreen screen, int tabId, ICoreGame game, ICorePlayer player) : base(screen, tabId, game, player)
